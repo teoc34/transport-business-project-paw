1244bd9236e5abc7044674c67ad5af140eb2d43b
Jacob <Jacob@DESKTOP-1RQC2SU>
2024-05-13 19:55:01 +0300

    Add project files.

---
+++ transport-business-project/Form1.cs
@@ -0,0 +1,10 @@
+namespace transport_business_project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ transport-business-project/Transport Business/Classes/Transport.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace transport_business_project.Classes
+{
+    internal class Transport
+    {
+    }
+}
